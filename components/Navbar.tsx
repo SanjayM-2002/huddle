c8a499b964124c4bc7113ea9174478d7fd2e1ad4
SanjayM-2002 <sanjaymshivakripa@gmail.com>
2024-05-29 09:56:40 +0530

    added homepage and sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+        <Link href='/' className='flex items-center gap-1'>
+          <Image
+            src='/icons/logo.svg'
+            width={32}
+            height={32}
+            alt='yoom logo'
+            className='max-sm:size-10'
+          />
+          <p className='text-[26px] font-extrabold text-white max-sm:hidden'>
+            Huddle
+          </p>
+        </Link>
+        <div className='flex-between gap-5'>{/*add user logged in */}</div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
